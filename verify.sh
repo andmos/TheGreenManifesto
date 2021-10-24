a71a6a40b3e40efc4621c01b1a33914555bee145
andmos <andreas.mosti@gmail.com>
2021-10-24 18:03:59 +0200

    removed interactive mode in container command

--- verify.sh
+++ verify.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker run --rm -v $(pwd):/usr/src/app/files -it andmos/markdownlint *.md
-docker run --rm -v $(pwd):/mnt -it andmos/awesome-bot -f *.md --allow-redirect --allow 429 --allow-ssl
+docker run --rm -v $(pwd):/usr/src/app/files andmos/markdownlint *.md
+docker run --rm -v $(pwd):/mnt andmos/awesome-bot -f *.md --allow-redirect --allow 429 --allow-ssl
