216e19c8f2dca192b5af1e9511a23c4bc2efdcbe
andmos <andreas.mosti@gmail.com>
2022-06-27 19:49:22 +0200

    Make script exit on failure

--- verify.sh
+++ verify.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+set -e
+
 docker run --rm -v $(pwd):/workdir ghcr.io/andmos/markdownlint-cli:master "*.md"
 docker run --rm -v $(pwd):/mnt andmos/awesome-bot -f *.md --allow-redirect --allow 429 --allow-ssl --white-list "nasdaq.com"
