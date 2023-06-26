9011e2c67b8e06aaf43f9082213d801f1288c920
Andreas Mosti <andreas.mosti@gmail.com>
2023-06-26 09:05:22 +0200

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker run --rm -v $(pwd):/workdir ghcr.io/andmos/markdownlint-cli:master "*.md"
-docker run --rm -v $(pwd):/mnt andmos/awesome-bot -f *.md --allow-redirect --allow 429 --allow-ssl --white-list "nasdaq.com,researchgate.net"
+docker run --rm -v $(pwd):/mnt andmos/awesome-bot -f *.md --allow-redirect --allow 429 --allow-ssl --white-list "nasdaq.com,researchgate.net" --skip-save-results 
